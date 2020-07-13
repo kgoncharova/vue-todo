31cb2774695e2c0395216707a6b6d3aa62e3a9dc
Kristina <codingkris@yahoo.com>
2020-07-13 12:19:09 +0300

    Add TodoList

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,17 @@
 <template>
   <div id="app">
-
+    <h1>Todo application</h1>
+    <hr>
+    <TodoList />
   </div>
 </template>
 
 <script>
-
+import TodoList from '@/components/TodoList.vue'
 export default {
   name: 'App',
   components: {
-
+    TodoList
   }
 }
 </script>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="todo-list">
+    <b-list-group>
+      
+    </b-list-group>
+  </div>
+</template>
+
+<style scoped>
+.todo-list {
+  margin: 0 auto;
+  max-width: 600px;
+  text-align: left;
+}
+</style>
