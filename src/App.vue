14f90a45853dd985f214b40647710d74065f4fa0
Kristina <codingkris@yahoo.com>
2020-07-13 16:29:15 +0300

    Fake data for testing

--- src/App.vue
+++ src/App.vue
@@ -27,6 +27,13 @@ export default {
       ]
     }
   },
+  mounted() {
+    fetch('https://jsonplaceholder.typicode.com/todos?_limit=10')
+      .then(response => response.json())
+      .then(json => {
+        this.todos = json
+      })
+  },
   methods: {
     removeTodo(id) {
       this.todos = this.todos.filter(t => t.id !== id)
